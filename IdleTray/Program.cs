082d947d5cb33affbbfc28968040ae692e4af706
erwinkarim <erwinkarim@gmail.com>
2012-11-28 10:35:44 +0800

    run one copy only

--- IdleTray/Program.cs
+++ IdleTray/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace IdleTray
 {
@@ -9,6 +10,8 @@ namespace IdleTray
     {
 
         public static IdleTrayAppContext idleTrayAppContext;
+        static String _mutexID = "8KeSLUeOEEIamgDf-DEvMzpXvPRVyfQoi";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,8 +20,23 @@ namespace IdleTray
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            idleTrayAppContext = new IdleTrayAppContext();
-            Application.Run(idleTrayAppContext);
+
+            Boolean _isNotRunning;
+            using (Mutex _mutex = new Mutex(true, _mutexID, out _isNotRunning))
+            {
+                if (_isNotRunning)
+                {
+                    //single instance of the app is running
+                    idleTrayAppContext = new IdleTrayAppContext();
+                    Application.Run(idleTrayAppContext);
+                }
+                else
+                {
+                    MessageBox.Show("An instance of Idle Client is already running");
+                    return;
+                }
+            }
+            
         }
     }
 }
---
+++ IdleTray/bin/Debug/app.publish/Application Files/IdleTray_1_0_0_11/ReadMe.txt.deploy
@@ -0,0 +1,11 @@
+﻿IdleTray Client for Windows
+
+== Intro
+This is Idle Tray Client for Windows, a minimal application sending idle information to a Fireworks License monitoring server.
+Default timer interval is 10 minutes. 
+
+== Requirements
+.Net framework version 4. Tested to work on Windows Vista, Windows 7
+
+== Building
+You can use MS Visual Studio to build this project and run it.
